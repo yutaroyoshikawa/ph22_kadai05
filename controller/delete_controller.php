120779fdedf52e84a4d3c22809aa27d14a980a2c
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2018-07-26 11:16:34 +0900

    [FIX]delete method

--- controller/delete_controller.php
+++ controller/delete_controller.php
@@ -1,6 +1,10 @@
 <?php
 require_once('../model/delete_model.php');
 
+$file_name = glob('../images/' . $_POST["id"] . '.*');
+
+unlink('../images/' . $file_name[0]);
+
 delete_img($_POST['id']);
 
 header('Location: ../view/list.php', true, 301);
--- model/delete_model.php
+++ model/delete_model.php
@@ -3,7 +3,7 @@ require_once('db_controll.php');
 
 function delete_img($id){
     $pdo = connect_db();
-    $stmt = $pdo->prepare('UPDATE image_file SET publish = 2 WHERE id = :id;');
+    $stmt = $pdo->prepare('DELETE FROM image_file WHERE id = :id;');
     $stmt->bindParam(':id', $id);
     $stmt->execute();
 }
